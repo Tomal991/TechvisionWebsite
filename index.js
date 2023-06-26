9acddfb32758ee33abda9a3541ce8fad10cf75b8
Tomal991 <intesarulhaquey4@gmail.com>
2023-06-26 15:51:37 +0600

    website done

--- index.js
+++ index.js
@@ -14,3 +14,20 @@ items.forEach((el) => {
     }
 })
 
+function sendEmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "intesarulhaquey4@gmail.com",
+        Password : "82B1ED0AFEE3C043527E2A10EC43E34A140C",
+        To : 'intesarul15-2170@diu.edu.bd',
+        From : document.getElementById('email').value,
+        Subject : document.getElementById('subject').value,
+        Body : 'Name:'+ document.getElementById('name').value
+         + '<br> phone:'+ document.getElementById('phone').value
+         + '<br> message:'+ document.getElementById('message').value
+         + '<br> address:'+ document.getElementById('address').value
+    }).then(
+      message => alert(message)
+    );
+  }
+  
